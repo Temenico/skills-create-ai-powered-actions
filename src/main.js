c73fb11926c6bac34b3bb2ceb9ad58223cc6190f
Nicolas Tello Mendez <148289845+Temenico@users.noreply.github.com>
2026-04-09 20:48:08 -0500

    Complete AI-powered joke rating action workflow

---
+++ src/main.js
@@ -0,0 +1,17 @@
+const { rateJoke } = require("./rateJoke");
+const core = require("@actions/core");
+
+async function run() {
+  try {
+    const joke = core.getInput("joke", { required: true });
+    const token = core.getInput("token", { required: true });
+
+    const rating = await rateJoke(joke, token);
+
+    core.setOutput("result", JSON.stringify(rating));
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+module.exports = { run };
